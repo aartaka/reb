36319700ba2d7177eee4dddc2b5e7103daebc4b1
Artyom Bologov <aartaka@protonmail.com>
2024-03-17 03:02:57 +0400

    reb: Multiple fixes and debug printing removal.

--- reb.c
+++ reb.c
@@ -43,28 +43,23 @@ void replace_pattern (char *str, struct optimization opt) {
         regex_t *reg = malloc(sizeof(regex_t));
         size_t rmax = 1000;
         regmatch_t rmatch[rmax];
-        char buf[200];
+        char buf[10000];
         strcpy(buf, str);
         regcomp(reg, opt.pattern, 0);
         while (!regexec(reg, str, rmax, rmatch, 0)) {
                 size_t buf_idx = 0;
-                printf("Another iteration: %s", str);
                 for (size_t i = 0; i < strlen(opt.replacement); ++i) {
                         if (opt.replacement[i] < 15) {
-                                printf("Replacing with substitution %s\n", opt.pattern);
-                                regmatch_t match = rmatch[opt.replacement[i]];
+                                regmatch_t match = rmatch[(size_t)opt.replacement[i]];
                                 strcpy(buf + buf_idx, str + match.rm_so);
                                 buf_idx += match.rm_eo - match.rm_so;
                         } else if (15 < opt.replacement[i] && opt.replacement[i] < ' ') {
                                 regmatch_t match = rmatch[opt.replacement[i] - 15];
-                                printf("Replacing with length %d\n", match.rm_eo - match.rm_so);
                                 buf_idx += sprintf(buf + buf_idx, "%d", match.rm_eo - match.rm_so);
                         } else {
-                                printf("Replacing with char %c\n", opt.replacement[i]);
                                 buf[buf_idx++] = opt.replacement[i];
                         }
                 }
-                printf("Buffer is %s at %d\n", buf, buf_idx);
                 memcpy(str + rmatch[0].rm_so, buf, buf_idx);
                 strcpy(str + rmatch[0].rm_so + buf_idx, str + rmatch[0].rm_eo);
         }
