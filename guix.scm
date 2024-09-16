7825071de573e4b24d219687939f5d9f766dec2c
Artyom Bologov <git@aartaka.me>
2024-09-16 15:47:46 +0400

    guix.scm: Add.

---
+++ guix.scm
@@ -0,0 +1,27 @@
+(use-modules (guix build-system gnu))
+(use-modules ((guix licenses) #:prefix license:))
+(use-modules (guix packages))
+(use-modules (guix gexp))
+
+(define-public reb
+  (package
+    (name "reb")
+    (version "0.0.1")
+    (source (local-file (dirname (current-filename)) #:recursive? #t))
+    (build-system gnu-build-system)
+    (synopsis "Regex-based toolkit for Brainfuck")
+    (description "Regex-based toolkit for Brainfuck")
+    (home-page "https://github.com/aartaka/reb")
+    (arguments
+     '(#:tests? #f
+       #:phases (modify-phases %standard-phases
+                  (delete 'configure)
+                  (replace 'install
+                    (lambda* (#:key outputs #:allow-other-keys)
+                      (let* ((out (assoc-ref outputs "out"))
+                             (bin (string-append out "/bin")))
+                        (mkdir-p bin)
+                        (install-file "reb" bin)))))))
+    (license license:wtfpl2)))
+
+reb
